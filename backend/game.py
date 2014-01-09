4782eda69c238c5542e06b17f857154e76b13214
Timothy Humphries <tim@utf8.me>
2014-01-09 18:28:21 +1100

    broken control token

--- backend/game.py
+++ backend/game.py
@@ -1,3 +1,4 @@
+import gevent
 from socketio.namespace import BaseNamespace
 from socketio.mixins import BroadcastMixin
 from socketio import socketio_manage
@@ -7,27 +8,48 @@ from backend import app
 
 import random
 import string
+import time
+
+roundlength = 60
 
 # Load word file
 # Not the best place to put this
 WORDFILE = "./untitled/backend/resource/words"
 words = []
 
-clients = 0
-artist = 1
+num_clients = 0
+artist = 0
 
 class PictNamespace(BaseNamespace, BroadcastMixin):
+    last_ping = 0
+    last_pong = 0
+
     def initialize(self):
-        global clients
+        global num_clients
+        global artist
         self.logger = app.logger
         self.log("Socketio session commence")
-        clients += 1
-        self.client_id = clients
+        num_clients += 1
+        self.client_id = num_clients
         self.emit('set_client_id', {'client_id': self.client_id})
         self.log("Sent client id %d" % self.client_id)
 
     def log(self, message):
         self.logger.info("[{0}] {1}".format(self.socket.sessid, message))
+
+
+    def on_request_control(self, posted):
+        global artist
+        global roundlength
+        if artist == 0:
+            artist = self.client_id
+            self.broadcast_event('grant_control', {'client_id': self.client_id})
+            gevent.sleep(roundlength)
+            artist = 0
+            self.broadcast_event('revoke_control', {'client_id': self.client_id})
+        else:
+            self.emit('deny_control', {})
+
 
     def on_post_drawing(self, posted):
         global artist
