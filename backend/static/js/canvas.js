5453ae833516c33d7a8853ce8f12398f569913af
Andrew C <andrewpcoleman@gmail.com>
2014-01-09 14:19:47 +1100

    began serialising

--- backend/static/js/canvas.js
+++ backend/static/js/canvas.js
@@ -1,9 +1,27 @@
-var canvas, ctx, flag = false;
+var canvas, ctx, isDrawing = false;
 var prevX, preyY, currX, currY;
 prevX = prevY = currX = currY = 0;
 var dot_flag = false;
-var x = "black";
-var y = 2;
+var colour = "black";
+var strokeWidth = 2;
+
+function Point(prevX, prevY, currX, currY) {
+  this.prevX = prevX;
+  this.prevY = prevY;
+  this.currX = currX;
+  this.currY = currY;
+}
+
+function Drawing() {
+  this.points = [];
+}
+
+Drawing.prototype.addPoint = function(point) {
+  this.points.push(point);
+}
+
+var drawings = [];
+var currentDrawing;
 
 function init() {
   canvas = document.getElementById("canvas");
@@ -29,8 +47,8 @@ function draw() {
   ctx.beginPath();
   ctx.moveTo(prevX, prevY);
   ctx.lineTo(currX, currY);
-  ctx.strokeStyle = x;
-  ctx.lineWidth = y;
+  ctx.strokeStyle = colour;
+  ctx.lineWidth = strokeWidth;
   ctx.stroke();
   ctx.closePath();
 }
@@ -39,32 +57,48 @@ function findxy(res, e) {
   var div = document.getElementById("pos")
   console.log(res);
   if (res == "down") {
+    // new drawing object
+    currentDrawing = new Drawing();
     prevX = currX;
     prevY = currY;
     currX = e.clientX - canvas.offsetLeft;
     currY = e.clientY - canvas.offsetTop;
 
-    flag = true;
+    isDrawing = true;
     dot_flag = true;
     if (dot_flag) {
       ctx.beginPath();
-      ctx.fillStyle = x;
+      ctx.fillStyle = colour;
       ctx.fillRect(currX, currY, 2, 2); 
       ctx.closePath();
       dot_flag = false;
     }
   }
   if (res == "up" || res == "out") {
-    flag = false;
+    isDrawing = false;
+    drawings.push(currentDrawing);
+    console.log("added drawing " + JSON.stringify(currentDrawing));
+    // finalise drawing object
   }
   if (res == "move") {
-    if (flag) {
+    if (isDrawing) {
       prevX = currX;
       prevY = currY;
       currX = e.clientX - canvas.offsetLeft;
       currY = e.clientY - canvas.offsetTop;
+      p = new Point(prevX, prevY, currX, currY);
+      console.log("added point " + JSON.stringify(p));
+      currentDrawing.addPoint(p);     
       draw();
     }
   }
   pos.innerHTML = "prevX: " + prevX + "<br/>prevY: " + prevY + "<br/>currX: " + currX + "<br/>currY: " + currY
+}
+
+function replay_draw() {
+
+}
+
+function replay(drawing) {
+  
 }
